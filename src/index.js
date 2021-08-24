e10b6dc64f7f0aff0e436fb54834bac3a84c3089
Katya Vinskovska <katyavinskovska@MacBook-Air-Katya.local>
2021-08-24 23:27:38 +0300

    init: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,40 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    console.log(bracketsConfig.length);
+    
+    let OPEN_BRACKETS = [];
+    let CLOSE_BRACKETS = {};
+    for (let i = 0; i < bracketsConfig.length; i++) {
+      OPEN_BRACKETS.push(bracketsConfig[i][0]);
+      CLOSE_BRACKETS[bracketsConfig[i][1]] = bracketsConfig[i][0];
+    }
+    console.log(OPEN_BRACKETS);
+    console.log(CLOSE_BRACKETS);
+    
+    let stack = [];
+    for (let i = 0; i < str.length; i++) {
+      if (stack.length > 0 && stack[stack.length - 1] === str[i] && str[i] === CLOSE_BRACKETS[str[i]]) {
+          stack.pop();
+      } else if (OPEN_BRACKETS.includes(str[i])) {
+        stack.push(str[i]);
+      } else {
+          if (stack.length === 0) {
+          return false;
+        }
+  
+        if (stack[stack.length - 1] === CLOSE_BRACKETS[str[i]]) {
+          stack.pop();
+        }
+          else {
+            return false;
+          }
+      }
+     
+      
+    }
+    
+    return stack.length === 0;
+    
+    
+  }
+
+
